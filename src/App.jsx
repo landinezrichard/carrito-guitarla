1b38e3e0e203ef56690a51ae08a2b33654626802
Ricardo <landinezrichard@gmail.com>
2026-03-20 15:10:28 -0500

    feat: empezando proyecto carro de compras ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+
+import { db } from "./data/db.js";
+
+function App() {
+
+  const guitars = db.map(guitar => <Guitar key={guitar.id} {...guitar} />);
+
+  return (
+    <>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">         
+
+          {guitars}
+
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
