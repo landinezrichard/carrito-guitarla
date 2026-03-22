f5e50d3aeed3ac00d8eb2af6d47c953a8ab541d8
Ricardo <landinezrichard@gmail.com>
2026-03-21 19:25:27 -0500

    fix: ajuste ruta imagenes css y componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = ({
             <a href="index.html">
               <img
                 className="img-fluid"
-                src="./public/img/logo.svg"
+                src="/img/logo.svg"
                 alt="imagen logo"
               />
             </a>
@@ -31,7 +31,7 @@ const Header = ({
             <div className="carrito">
               <img
                 className="img-fluid"
-                src="./public/img/carrito.png"
+                src="/img/carrito.png"
                 alt="imagen carrito"
               />
 
