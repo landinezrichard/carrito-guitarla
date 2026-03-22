aa02930f3233357474df6c5d652b686a30b017c2
Ricardo <landinezrichard@gmail.com>
2026-03-21 19:12:33 -0500

    feat: carro persistente con localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
 
 import { db } from "./data/db.js";
 
 function App() {
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+
   const [data, setData] = useState(db);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(initialCart);
 
   const MAX_QUANTITY = 5;
   const MIN_QUANTITY = 1;
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   function addToCart(item) {
     const itemExist = cart.findIndex((itemInCart) => itemInCart.id === item.id);
